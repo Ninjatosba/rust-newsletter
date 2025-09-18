48dd5c7ced7cf8d4359f6834228ddd5e220b5f81
Ninjatosba <adenizcorlu@gmail.com>
2025-09-19 02:06:21 +0300

    config init

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use newsletter::{configuration::get_configuration, startup::run};
 use std::net::TcpListener;
-
-use newsletter::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let configuration =
+        get_configuration().unwrap_or_else(|e| panic!("Failed to read configuration: {}", e));
     let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind port 8080");
     run(listener)?.await
 }
