d0d054088a556f823603d6c9675a2859e95570e4
Ninjatosba <adenizcorlu@gmail.com>
2026-02-07 19:55:25 +0300

    initilize logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use env_logger::Env;
 use newsletter::{configuration::get_configuration, startup::run};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration =
         get_configuration().unwrap_or_else(|e| panic!("Failed to read configuration: {}", e));
     let address = format!("127.0.0.1:{}", configuration.application_port);
